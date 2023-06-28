ff36023bbf6fd9d081bf70e0cceca0b0c120b142
Chetan Sengar <nathan4drake1999@gmail.com>
2023-06-28 13:45:28 +0530

    button width change

--- src/app/components/HomeNavButton.jsx
+++ src/app/components/HomeNavButton.jsx
@@ -8,7 +8,7 @@ const HomeNavButton = ({ text, url }) => {
       href={url}
       className="bg-[#D9D9D9] font-bold px-4 py-2 rounded-lg mx-auto flex justify-center my-6"
     >
-      <div className="w-2/4 flex items-center gap-4">
+      <div className="w-7/12 flex items-center gap-4">
         <Image
           src="/homenavbutton.svg"
           alt={`link to ${text} page`}
