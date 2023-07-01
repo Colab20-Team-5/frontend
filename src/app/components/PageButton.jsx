3b8119c61cdcaf24457874bba94507df2cf95947
IBUNHABIBU <ibunhabib@gmail.com>
2023-07-01 11:12:29 +0300

    Style the before gym

--- src/app/components/PageButton.jsx
+++ src/app/components/PageButton.jsx
@@ -3,9 +3,9 @@ import React from "react";
 
 const PageButton = ({ text, url }) => {
   return (
-    <Link href={url}>
-      <div className="w-3/4 bg-[#D9D9D9] text-sm mx-auto px-3 py-4 text-center rounded-xl my-6">
-        <button>{text}</button>
+    <Link href={url} className="btn btn--nav">
+      <div className="">
+        <button className="btn btn--nav">{text}</button>
       </div>
     </Link>
   );
