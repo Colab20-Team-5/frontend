bdcf48d981c8d8ecf4355a88a9eaeaee2f07a1ae
IBUNHABIBU <ibunhabib@gmail.com>
2023-07-01 10:40:48 +0300

    Finish the nav

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="fixed bottom-0 w-full bg-[#000000bf] px-4 py-3 flex justify-between">
+    <nav className="nav">
       {/* placeholders images, will be replaced with actual images and links */}
       <Link href="/">
         <Image src="/home.svg" alt="home nav icon" width={25} height={25} />
