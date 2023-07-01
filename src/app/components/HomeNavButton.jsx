107776260468acffd32bb0bf5812742bd118535e
IBUNHABIBU <ibunhabib@gmail.com>
2023-07-01 09:12:56 +0300

    Finish the home page

--- src/app/components/HomeNavButton.jsx
+++ src/app/components/HomeNavButton.jsx
@@ -6,9 +6,9 @@ const HomeNavButton = ({ text, url }) => {
   return (
     <Link
       href={url}
-      className="bg-[#D9D9D9] font-bold px-4 py-2 rounded-lg mx-auto flex justify-center my-6 btn"
+      className="btn btn--home-nav"
     >
-      <div className="w-7/12 flex items-center gap-4">
+      <div className="img-container">
         <Image
           src="/homenavbutton.svg"
           alt={`link to ${text} page`}
