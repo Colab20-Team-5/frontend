973786ec99754fd5db059453903604b21f395edf
IBUNHABIBU <ibunhabib@gmail.com>
2023-07-02 19:59:14 +0300

    Add music styling

--- src/app/beforegym/guidedaudio/page.jsx
+++ src/app/beforegym/guidedaudio/page.jsx
@@ -11,8 +11,18 @@ const page = () => {
         height={40}
       />
       <h3>Guided Audio Workout</h3>
+      <Image
+        src="/play-1001-svgrepo-com.svg"
+        alt="Play arrow big"
+        width={80}
+        height={80}
+      />
     </div>
     <div className="audio__controls">
+      <audio controls>
+      <source src="h" type="audio/mpeg" />
+
+      </audio>
       <Image
         src="/previous-svgrepo-com.svg"
         alt="Prev svg"
