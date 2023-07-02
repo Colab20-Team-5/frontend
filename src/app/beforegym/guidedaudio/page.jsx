af10ee63bec787d7932f39bd938645f15ab3956e
IBUNHABIBU <ibunhabib@gmail.com>
2023-07-02 19:12:15 +0300

    Add audio icons

--- src/app/beforegym/guidedaudio/page.jsx
+++ src/app/beforegym/guidedaudio/page.jsx
@@ -1,7 +1,36 @@
+import Image from "next/image";
 import React from "react";
 
 const page = () => {
-  return <div>guided audio</div>;
+  return <div>
+    <div className="audio-title">
+      <Image
+        src="/back-svgrepo-com.svg"
+        alt="Back arrow"
+        width={40}
+        height={40}
+      />
+      <h3>Guided Audio Workout</h3>
+    </div>
+    <Image 
+      src="/previous-svgrepo-com.svg"
+      alt="Prev svg"
+      width={40}
+      height={40}
+    />
+     <Image 
+      src="/previous-svgrepo-com.svg"
+      alt="Prev svg"
+      width={40}
+      height={40}
+    />
+     <Image 
+      src="/previous-svgrepo-com.svg"
+      alt="Prev svg"
+      width={40}
+      height={40}
+    />
+  </div>;
 };
 
 export default page;
