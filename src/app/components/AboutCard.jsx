fa46527daf6fd58f0e10a530ad7603eee7058050
IBUNHABIBU <ibunhabib@gmail.com>
2023-07-05 11:24:07 +0300

    Add about style

--- src/app/components/AboutCard.jsx
+++ src/app/components/AboutCard.jsx
@@ -4,20 +4,22 @@ import cardDetails from '../constants';
 const AboutCard = () => {
   return (
     <>
-    <section className="section">
-      <h2>ABOUT US</h2>
-      <p>Your personalized fitness journey,
+    <section className="about">
+      <h2 className="about__title">ABOUT US</h2>
+      <p className="about__text">Your personalized fitness journey,
           empowering you to achieve your fitness goals with a tailor-made
             workout plan and seamless tracking on the go.
       </p>
     </section>
-    <section className='section-about'>
+    <section className='section-card'>
       {
         cardDetails.map(({im, title, description}) => {
             return (
                 <>
-                    <h2>{title}</h2>
+                    <div className="card">
+                      <h2>{title}</h2>
                       <p>{description}</p>
+                    </div>
                 </>
             )
         })
