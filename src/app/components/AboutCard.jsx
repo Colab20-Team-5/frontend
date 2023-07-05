42039fa3076eab02b6c187cc9812c89004c0c087
IBUNHABIBU <ibunhabib@gmail.com>
2023-07-05 09:24:33 +0300

    Add hero section

--- src/app/components/AboutCard.jsx
+++ src/app/components/AboutCard.jsx
@@ -4,11 +4,13 @@ import cardDetails from '../constants';
 const AboutCard = () => {
   return (
     <>
-        <section className="section-about">
+        <section className="section-hero">
           <h2>ABOUT US</h2>
           <p>Your personalized fitness journey,
             empowering you to achieve your fitness goals with a tailor-made
              workout plan and seamless tracking on the go.</p>
+        </section>
+    <section className='section-about'>
 {
     cardDetails.map(({im, title, description}) => {
         return (
