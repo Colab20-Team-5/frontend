23f2ade037aeb9389c8af36f27db1b7ff34ef49b
Chetan Sengar <nathan4drake1999@gmail.com>
2023-07-05 19:00:32 +0530

    customize plan & continue button added

--- src/app/beforegym/customizeworkoutplan/page.jsx
+++ src/app/beforegym/customizeworkoutplan/page.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import HeaderText from "../../components/HeaderText";
+import PageButton from "@/app/components/PageButton";
 
 const page = () => {
   return (
@@ -9,6 +10,33 @@ const page = () => {
         secondHeader={"Plan My Workout"}
         paraText={"Customize your 45 minute workouts. "}
       />
+
+      <div className="customize-plan-container">
+        <div className="day">
+          <p>Monday</p>
+          <p>+</p>
+        </div>
+        <div className="day">
+          <p>Tuesday</p>
+          <p>+</p>
+        </div>
+        <div className="day">
+          <p>Thursday</p>
+          <p>+</p>
+        </div>
+        <div className="day">
+          <p>Friday</p>
+          <p>+</p>
+        </div>
+      </div>
+
+      <div className="continue-btn">
+        <PageButton
+          text={"Map to Gym"}
+          url={"/beforegym/maptogym"}
+          arrow={"/nextarrow.svg"}
+        />
+      </div>
     </div>
   );
 };
