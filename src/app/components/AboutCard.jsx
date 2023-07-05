596e98c70052df5f245b95ecae7a97e850e8436c
IBUNHABIBU <ibunhabib@gmail.com>
2023-07-05 09:07:13 +0300

    Add card details constants

--- src/app/components/AboutCard.jsx
+++ src/app/components/AboutCard.jsx
@@ -1,7 +1,26 @@
-<section className="section-about">
+import React from 'react'
+
+const AboutCard = () => {
+  return (
+    <>
+        <section className="section-about">
           <h2>ABOUT US</h2>
           <p>Your personalized fitness journey,
             empowering you to achieve your fitness goals with a tailor-made
              workout plan and seamless tracking on the go.</p>
-             
-</section>
+{
+    cardDetails
+}
+             <div className="card">
+
+             </div>
+
+        </section>
+    </>
+  )
+}
+
+export default AboutCard;
+
+
+
--- src/app/constants/index.js
+++ src/app/constants/index.js
@@ -6,13 +6,15 @@ const cardDetails = [
     },
     {
         im: "",
-        title: "A Personalized Fitness Adventure",
-        description: "Let's start this journey and unlock your full potential before the gym.  Get ready to crush your workouts and become the best version of yourself"
+        title: "Unleash your potential at Gym",
+        description: "Elevate your gym workouts with guided sessions, goal-oriented checklists, and video instructions. Crush your fitness goals confidently."
     },
     {
         im: "",
-        title: "A Personalized Fitness Adventure",
-        description: "Let's start this journey and unlock your full potential before the gym.  Get ready to crush your workouts and become the best version of yourself"
+        title: "Elevate your progress post Gym",
+        description: "Maxmize your post Gym result with tailored Guidence and Injury prevention tips"
     },
 
-]
+];
+
+export default cardDetails;
