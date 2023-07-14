05e56f4ca2bffb97172ba42cbc4e10a1eff26505
IBUNHABIBU <ibunhabib@gmail.com>
2023-07-14 16:39:45 +0300

    Add players icons

--- src/app/beforegym/guidedaudio/page.jsx
+++ src/app/beforegym/guidedaudio/page.jsx
@@ -1,9 +1,15 @@
+'use client'
+import React, { useState } from "react";
 import PageButton from "@/app/components/PageButton";
 import HeaderText from "../../components/HeaderText";
-import React from "react";
 import Image from "next/image";
 
 const page = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const handleClick = () => {
+    setIsPlaying(!isPlaying);
+  };
+
   return (
     <div className="audio">
       <HeaderText
@@ -19,12 +25,25 @@ const page = () => {
         <p>Motivational Workout</p>
       </div>
       <div className="audio-player">
-        <div className="audio-player--icons">
-          <Image src="/previous-svgrepo-com.svg" width={130} height={130} />
-          <Image src="/play-1001-svgrepo-com.svg" width={130} height={130} />
-          <Image src="/pause-1010-svgrepo-com.svg" width={130} height={130} />
-          <Image src="/next-svgrepo-com.svg" width={130} height={130} />
-          <input type="range" name="" id="" />
+        <div className="audio-player__icons">
+          <Image src="/previous-svgrepo-com.svg" width={60} height={60} className="icon"/>
+          { isPlaying ?  (<Image
+            src="/play-1001-svgrepo-com.svg"
+            width={100}
+            height={100}
+            className="icon"
+            onClick = { handleClick } />) :
+          (<Image
+            src="/pause-1010-svgrepo-com.svg"
+            width={100}
+            height={100}
+            className="icon"
+            onClick = { handleClick }
+            />)
+          }
+
+          <Image src="/next-svgrepo-com.svg" width={60} height={60} />
+          <input type="range" name="progress time" id="progress" />
         </div>
         <audio controls>
           <source src="/audio.mp3" type="audio/mp3" />
