429af9f85a4a82ced6003c30c3363fb596480ccc
IBUNHABIBU <ibunhabib@gmail.com>
2023-07-20 19:51:51 +0300

    Add at gym header

--- src/app/atgym/page.jsx
+++ src/app/atgym/page.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
+import HeaderImg from '../components/HeaderImg';
 
 const page = () => {
   return (
     <div>
-        <h2>At gym</h2>
+        <HeaderImg headerImg={"/atgym.png"} />
+        <div className="at-gym-header">
+            <h2>At gym</h2>
+            <p>You’ve made it to the gym!
+            Choose your week of workout to begin.</p>
+        </div>
+
+        <div className="accordion">
+            <div className="accordion__item">
+                <p>Select week</p>
+                <div className="accordion__item-body">
+                    <div className="accordion-item-body-content">
+                        <p>Week 1</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
 
-export default page
+export default page;
