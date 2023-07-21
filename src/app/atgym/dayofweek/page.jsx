4632fc627de21f8c416eb3b9881f6f30512438b1
IBUNHABIBU <ibunhabib@gmail.com>
2023-07-21 23:28:03 +0300

    Day of week plan'

--- src/app/atgym/dayofweek/page.jsx
+++ src/app/atgym/dayofweek/page.jsx
@@ -1,13 +1,39 @@
+import HeaderText from '@/app/components/HeaderText'
+import HomeNavButton from '@/app/components/HomeNavButton'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>
+    <div className=>
         <HeaderText
-        headerText={"Journey to the Gym"}
-        secondHeader={"Listen to your Music"}
-        paraText={"Integrate your music with your journey."}
+        headerText={"At the Gym"}
+        paraText={"Choose the day of your week to workout"}
       />
+
+      <HomeNavButton 
+        text={"Monday"}
+        url={"/atgym/spotify"}
+        arrow={"/nextarrow.svg"}
+      />
+
+     <HomeNavButton 
+        text={"Wednesday"}
+        url={"/atgym/spotify"}
+        arrow={"/nextarrow.svg"}
+      />
+
+    <HomeNavButton 
+        text={"Thursday"}
+        url={"/atgym/spotify"}
+        arrow={"/nextarrow.svg"}
+      />
+
+      <HomeNavButton 
+        text={"Thursday"}
+        url={"/atgym/spotify"}
+        arrow={"/nextarrow.svg"}
+      />
+
     </div>
   )
 }
