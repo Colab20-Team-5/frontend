9ead9e12747ee7bf61f62beaf6ef341d45663197
IBUNHABIBU <ibunhabib@gmail.com>
2023-07-21 23:23:11 +0300

    Day of week plan'

---
+++ src/app/atgym/dayofweek/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <HeaderText
+        headerText={"Journey to the Gym"}
+        secondHeader={"Listen to your Music"}
+        paraText={"Integrate your music with your journey."}
+      />
+    </div>
+  )
+}
+
+export default page
