a85d77e48c6b18f08ab1c219592e8179c3be7ce1
IBUNHABIBU <ibunhabib@gmail.com>
2023-07-21 23:37:47 +0300

    Add days

--- src/app/atgym/dayofweek/page.jsx
+++ src/app/atgym/dayofweek/page.jsx
@@ -4,35 +4,36 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className=>
+    <div>
         <HeaderText
         headerText={"At the Gym"}
         paraText={"Choose the day of your week to workout"}
       />
 
-      <HomeNavButton 
-        text={"Monday"}
-        url={"/atgym/spotify"}
-        arrow={"/nextarrow.svg"}
-      />
-
-     <HomeNavButton 
-        text={"Wednesday"}
-        url={"/atgym/spotify"}
-        arrow={"/nextarrow.svg"}
-      />
-
-    <HomeNavButton 
-        text={"Thursday"}
-        url={"/atgym/spotify"}
-        arrow={"/nextarrow.svg"}
-      />
-
-      <HomeNavButton 
-        text={"Thursday"}
-        url={"/atgym/spotify"}
-        arrow={"/nextarrow.svg"}
-      />
+      <div className="btn-container">
+        <HomeNavButton
+          text={"Monday"}
+          url={"/atgym/spotify"}
+          arrow={"/nextarrow.svg"}
+        />
+        
+             <HomeNavButton
+          text={"Wednesday"}
+          url={"/atgym/spotify"}
+          arrow={"/nextarrow.svg"}
+        />
+        
+            <HomeNavButton
+          text={"Thursday"}
+          url={"/atgym/spotify"}
+          arrow={"/nextarrow.svg"}
+        />
+        <HomeNavButton
+          text={"Thursday"}
+          url={"/atgym/spotify"}
+          arrow={"/nextarrow.svg"}
+        />
+      </div>
 
     </div>
   )
