6c6666407c0edc428cbbd36d7de092016ff1f07f
IBUNHABIBU <ibunhabib@gmail.com>
2023-07-21 22:01:42 +0300

    Add accordion

--- src/app/atgym/accordion.jsx
+++ src/app/atgym/accordion.jsx
@@ -8,70 +8,34 @@ import Image from "next/image";
 const Accordion = () => {
   const [clickedDays, setClickedDays] = useState([]);
   const [text, setText] = useState("");
+  const [isClicked, setIsClicked] = useState(false);
 
-  const handleClick = (dayId) => {
-    if (clickedDays.includes(dayId)) {
-      const newClickedDays = clickedDays.filter((id) => id !== dayId);
-      setClickedDays(newClickedDays);
-      return;
-    }
-
-    setClickedDays([...clickedDays, dayId]);
-  };
-
-  const handleChange = (e, dayId) => {
-    const updatedText = {
-      ...text,
-      [dayId]: e.target.value,
-    };
-    setText(updatedText);
-  };
-
-  const handleSubmit = (e, dayId) => {
-    e.preventDefault();
-  };
-
-  const handleKeyDown = (e, dayId) => {
-    if (e.keyCode === 13) {
-      e.preventDefault();
-      const updatedText = {
-        ...text,
-        [dayId]: (text[dayId] || "") + "\n",
-      };
-      setText(updatedText);
-    }
+  const handleClick = () => {
+    setIsClicked(!isClicked);
   };
 
   return (
     <div className="main-container">
   
       <div className="customize-plan-container">
-        {weekDays.map(({ id, day }) => {
-          const isClicked = clickedDays.includes(id);
-          return (
+        
             <div className="day-container">
-              <div className="day" key={id} onClick={() => handleClick(id)}>
-                <p>{day}</p>
+              <div className="day" onClick={() => handleClick()}>
+                <p>Monday June 26th 2023</p>
                 <button type="submit">
-                  {isClicked ? ( <Image
-                    src="/previo-svgrepo-com.svg"
+                  <Image
+                    src="/down-arrow-backup-2-svgrepo-com.svg"
                     width={30}
                     height={30}
                     className="icon icon--1"
-                />) : ( <Image
-                    src="/previous-svgrepo-com.svg"
-                    width={30}
-                    height={30}
-                    className="icon icon--1"
-                />)}
+                />
           </button>
               </div>
               {isClicked && (
                <h1>hello</h1>
               )}
             </div>
-          );
-        })}
+     
       </div>
 
     </div>
