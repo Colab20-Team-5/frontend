387e084ed4de6929f6bedaf463095ff7d6116c02
Chetan Sengar <nathan4drake1999@gmail.com>
2023-07-23 18:20:54 +0530

    no plan screen

--- src/app/atgym/layout.jsx
+++ src/app/atgym/layout.jsx
@@ -1,10 +1,25 @@
+"use client";
+import { getDataLS } from "@/utils";
 import HeaderImg from "../components/HeaderImg";
+import PageButton from "../components/PageButton";
 
 export default function AtGym({ children }) {
   return (
     <div>
       <HeaderImg headerImg={"/atgym.png"} />
-      <div className="container">{children}</div>
+      {getDataLS("week") ? (
+        <div className="container">{children}</div>
+      ) : (
+        <div className="noplan-container">
+          <div className="paragraph">
+            You haven't created any workout plan, Please create a workout plan
+            in Before Gym section.
+          </div>
+          <div className="continue-btn">
+            <PageButton url={"/beforegym"} text={"Create Workout Plan"} />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
